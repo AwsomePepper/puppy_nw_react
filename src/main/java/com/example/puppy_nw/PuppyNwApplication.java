ba4b9b2189700529d3c00ccf037e4a6d71c8c267
HanHoing <hhy6789@gmail.com>
2021-12-24 10:03:45 +0900

    first commit

---
+++ src/main/java/com/example/puppy_nw/PuppyNwApplication.java
@@ -0,0 +1,13 @@
+package com.example.puppy_nw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppyNwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppyNwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/puppy_nw/PuppyNwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.puppy_nw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppyNwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
